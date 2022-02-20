ca3ba57b228323192c9b68a8018315f9c3bbaf6c
MySeR <myserofficial@gmail.com>
2022-02-20 14:13:57 +0000

    - Corrigido nome das imagens das séries.
    - Corrigido nome das imagens das séries "Peaky Blinders" e "La Casa de Papel".

--- code/ssms_queries/series/update_serie_images.sql
+++ code/ssms_queries/series/update_serie_images.sql
@@ -2,13 +2,13 @@ USE [WatchItDB];
 
 UPDATE dbo.Series
 	SET Imagem_Background='../static/images/series_background/peaky_blinders/peaky_blinders_background.png',
-	Imagem_Capa='../static/images/series_background/thumbnails/peakyblinders.jpg'
+	Imagem_Capa='../static/images/series_background/thumbnails/peaky_blinders.jpg'
 WHERE ID=1;
 
 
 UPDATE dbo.Series
-	SET Imagem_Background='../static/images/series_background/peaky_blinders/peaky_blinders_background.png',
-	Imagem_Capa='../static/images/series_background/thumbnails/peakyblinders.jpg'
-WHERE ID=1;
+	SET Imagem_Background='../static/images/series_background/la_casa_de_papel/la_casa_de_papel_background.jpg',
+	Imagem_Capa='../static/images/series_background/thumbnails/la_casa_de_papel.jfif'
+WHERE ID=2;
 
 SELECT * FROM dbo.Series;
