3d499fc94df6594d76ccd3fd8e39a4997ec6acf6
MySeR <myserofficial@gmail.com>
2022-02-22 09:16:38 +0000

    - Inseridos nomes e valores dos botões das temporadas.
    - Inseridos nomes e valores dos botões das temporadas;
    - Removidos comandos de SQL desnecssários na query "insert_new_episode.sql".

--- code/ssms_queries/series/insert_new_episode.sql
+++ code/ssms_queries/series/insert_new_episode.sql
@@ -1,12 +1,4 @@
 USE [WatchItDB];
 
-INSERT INTO dbo.Episodios VALUES('Peaky Blinders', 1, 3, '..static/images/series_background/peaky_blinders/S1/EP3/ep3.mp4', 
-								'../static/images/series_background/peaky_blinders/S1/EP3/ep3_background.jpg');
-
-
-SELECT
-    Video_Episodio
-FROM dbo.Episodios
-WHERE (Nome_Serie='Peaky Blinders' AND Num_Temporada=1 AND Num_Episodio=1)
-
-SELECT * FROM dbo.Episodios;
+INSERT INTO dbo.Episodios VALUES('Peaky Blinders', 1, 6, '..static/images/series_background/peaky_blinders/S1/EP6/ep6.mp4', 
+								'../static/images/series_background/peaky_blinders/S1/EP6/ep6_background.jpg');
