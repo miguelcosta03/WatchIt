900df48afe4917190d739d44e76306a049252af0
MySeR <myserofficial@gmail.com>
2022-02-23 21:22:00 +0000

    - Adicionado ficheiro SQL.
    - Adicionado ficheiro SQL para criar a tabela de número de episódios por temporada.

---
+++ code/ssms_queries/series/create_episodes_per_season_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE EpsPorTemporada(
+	ID INT IDENTITY(1,1) PRIMARY KEY,
+	ID_Serie INT,
+	Num_Eps_Temp_1 INT,
+	Num_Eps_Temp_2 INT,
+	Num_Eps_Temp_3 INT,
+	Num_Eps_Temp_4 INT,
+	Num_Eps_Temp_5 INT,
+	Num_Eps_Temp_6 INT,
+	Num_Eps_Temp_7 INT,
+	Num_Eps_Temp_8 INT,
+	Num_Eps_Temp_9 INT,
+	Num_Eps_Temp_10 INT);
