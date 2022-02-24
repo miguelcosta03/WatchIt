6bf121b61bb4b878320ec7e9930a71f0b1f46a7f
MySeR <myserofficial@gmail.com>
2022-02-24 11:20:03 +0000

    - Substituição do nome da série pelo ID da mesma.

--- code/ssms_queries/series/insert_new_episode.sql
+++ code/ssms_queries/series/insert_new_episode.sql
@@ -1,4 +1,4 @@
 USE [WatchItDB];
 
-INSERT INTO dbo.Episodios VALUES('Peaky Blinders', 1, 6, '..static/images/series_background/peaky_blinders/S1/EP6/ep6.mp4', 
-								'../static/images/series_background/peaky_blinders/S1/EP6/ep6_background.jpg');
+INSERT INTO dbo.Episodios VALUES(1, 5, 6, '../static/images/series_background/peaky_blinders/S5/EP6/ep6.mp4', 
+								'../static/images/series_background/peaky_blinders/S5/EP6/ep6_background.jpg');
