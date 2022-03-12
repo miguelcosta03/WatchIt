471d56036348e534317ee6761d027250602eec55
MySeR <myserofficial@gmail.com>
2022-03-12 12:11:19 +0000

    - Correção da condição da estrutura condicional.
    - Correção da condição da estrutura condicional do ficheiro JS para mostrar/esconder a password.

--- code/static/js/password.js
+++ code/static/js/password.js
@@ -1,6 +1,6 @@
 function showHidePassword() {
     pswField = window.document.getElementById('password')
-    if (pswField.type == 'password') {
+    if (pswField.type === 'password') {
         pswField.type = 'text'
     } else {
         pswField.type = 'password'
