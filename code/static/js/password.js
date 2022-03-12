ffc5f4271d06d330aa0367597a7d5ca949e1bdfa
MySeR <myserofficial@gmail.com>
2022-03-12 12:26:28 +0000

    - Adicionada campo de verificação de password à função para mostrar/esconder a password.

--- code/static/js/password.js
+++ code/static/js/password.js
@@ -1,8 +1,11 @@
 function showHidePassword() {
     pswField = window.document.getElementById('password')
+    confPswField = window.document.getElementById('confirm_password')
     if (pswField.type === 'password') {
         pswField.type = 'text'
+        confPswField.type = 'text'
     } else {
         pswField.type = 'password'
+        confPswField.type = 'password'
     }
 }
