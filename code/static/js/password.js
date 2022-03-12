c85098d71dfa93b73684d5956f09f39e0dfabeef
MySeR <myserofficial@gmail.com>
2022-03-12 12:07:44 +0000

    - Criado ficheiro JS para a operar com a checkbox da password.
    - Criado ficheiro JS para a operar com a checkbox da password.

---
+++ code/static/js/password.js
@@ -0,0 +1,8 @@
+function showHidePassword() {
+    pswField = window.document.getElementById('password')
+    if (pswField.type == 'password') {
+        pswField.type = 'text'
+    } else {
+        pswField.type = 'password'
+    }
+}
