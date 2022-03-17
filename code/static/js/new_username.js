844b7a2f3cf20570c9e29c93d00955f441a521a5
MySeR <myserofficial@gmail.com>
2022-03-17 20:50:58 +0000

    - Movida função de esconder div do username na página "Editar Perfil".
    - Movida função de esconder div do username na página "Editar Perfil" para o novo ficheiro JS "update_credentials.js" responsável por todas as operações frontend da parte de atualizar as informações do utilizador na página "Editar Perfil";
    - Criada função para esconder div do endereço de email na página "Editar Perfil".

--- code/static/js/new_username.js
+++
@@ -1,9 +0,0 @@
-function showHideNewUsername() {
-    let editButton = document.getElementById('edit-username-button')
-    let usernameDiv = document.getElementById('new-username-div')
-    if (usernameDiv.style.display === "block") {
-        usernameDiv.style.display = "none"
-    } else {
-        usernameDiv.style.display = "block"
-    }
-}
---
+++ code/static/js/update_credentials.js
@@ -0,0 +1,18 @@
+function showHideNewUsernameDiv() {
+    let usernameDiv = document.getElementById('new-username-div')
+    if (usernameDiv.style.display === "block") {
+        usernameDiv.style.display = "none"
+    } else {
+        usernameDiv.style.display = "block"
+    }
+}
+
+
+function showHideNewEmailDiv() {
+    let emailDiv = document.getElementById('new-email-div')
+    if (emailDiv.style.display === "block") {
+        emailDiv.style.display = "none"
+    } else {
+        emailDiv.style.display = "block"
+    }
+}
