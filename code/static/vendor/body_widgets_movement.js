c29401b6da35910f6417f1c7193740b6c9ca7ccb
Miguel <myserofficial@gmail.com>
2022-04-23 14:16:36 +0100

    - Adicionada nova função de boostrap.
    - Adicionada nova função de boostrap no body da página "Série". Esta função consiste em corrigir a posição dos botões respetivos ao número da temporada numa determinada resolução.

--- code/static/vendor/body_widgets_movement.js
+++ code/static/vendor/body_widgets_movement.js
@@ -1,3 +1,29 @@
+function moveSeasonButtons() {
+    let winWidth = window.innerWidth;
+    let season1Button = document.getElementById('season1Button');
+    let season2Button = document.getElementById('season2Button');
+    let season3Button = document.getElementById('season3Button');
+    let season4Button = document.getElementById('season4Button');
+    let season5Button = document.getElementById('season5Button');
+
+    if (winWidth >= 767 && winWidth <= 1183) {
+        season1Button.style.position = "relative";
+        season1Button.style.left = "-18vw";
+        season2Button.style.left = "-17vw";
+        season3Button.style.left = "-16vw";
+        season4Button.style.left = "-15vw";
+        season5Button.style.left = "-14vw";
+    } else {
+        season1Button.style.left = "-10vw";
+        season2Button.style.left = "-9vw";
+        season3Button.style.left = "-8vw";
+        season4Button.style.left = "-7vw";
+        season5Button.style.left = "-6vw";
+    }
+
+    console.log(winWidth)
+}
+
 function movePlayerButtons() {
     let winWidth = window.innerWidth;
     let normalPlayer = document.getElementById('normalPlayer');
@@ -18,4 +44,9 @@ function movePlayerButtons() {
         normalPlayer.style.marginLeft = "17vw";
         watchItPlayer.style.marginLeft = "24.2vw";
     }
+}
+
+function moveBodyWidgets() {
+    moveSeasonButtons();
+    movePlayerButtons();
 }
