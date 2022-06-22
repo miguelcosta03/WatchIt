e59eeb10b093e7765ad9eb3978ebaae37829c79d
miguelcosta03 <myserofficial@gmail.com>
2022-06-22 10:48:58 +0100

    - Alterado email de envio do código de confirmação da password.
    - Foi alterado o email de envio do código de confirmação da password.

--- code/contacts.py
+++ code/contacts.py
@@ -3,16 +3,13 @@ import smtplib
 class Contacts:
     def __init__(self, user_email):
         self.user_email = user_email
-        self.supportEmail = "watchitsatisfaction@gmail.com"
-        self.supportPassword = "sla123456#"
+        self.supportEmail = "noreply@infixa.pt"
 
     def send_email(self, verificationCode):
         emailMsg = f"""\
                     Código de Verificação
 
                     O teu código de verificação de alteração de password é: {verificationCode}"""
-        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-        server.login(self.supportEmail, self.supportPassword)
+        server = smtplib.SMTP('10.0.0.1')
         server.sendmail(self.supportEmail, self.user_email, emailMsg.encode("utf-8"))
-        server.close()
         return
